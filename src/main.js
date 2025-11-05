c09f23ab653325dae0f6183750b3c98f865feddc
Toka1000101 <tmaridasvili@protonmail.com>
2025-11-05 15:32:34 -0500

    add folding and animation

--- src/main.js
+++ src/main.js
@@ -3,19 +3,18 @@ import gsap from 'gsap';
 
 // aspect ration of A4 paper is sqrRoot(2) : 1
 // which is approximatelly 1.414:1
-const A4size = 1.414;
 function createPaper(size, options = {}) {
 	const {
 		segments = 10,
 		showGrid = true,
 		gridColor = 0x000000,
-		paperColor = 0xFFFFFF,
+		paperColor = 0x808080,
 		A4size = 1.414 // √2 ≈ A4 ratio
 	} = options;
 
-	const geom = new THREE.PlaneGeometry(size, size * A4size,10,10);
+	const geom = new THREE.PlaneGeometry(size, size * A4size, segments, segments);
 	const mat = new THREE.MeshBasicMaterial({
-		color: 0xFFFFFF,
+		color: paperColor,
 		side: THREE.DoubleSide,
 	});
 	
@@ -35,6 +34,7 @@ function createPaper(size, options = {}) {
 	paperGroup.userData.geom = geom;
 	return paperGroup;
 }
+
 function createaCreaseLine(p1, p2) {
 	let points = [
 		new THREE.Vector3(p1[0], 0, p1[1]),
@@ -53,13 +53,67 @@ function addPlaneCreaseLines(paper) {
 	let verticalCrease = createaCreaseLine([0, height/2],[0, -height/2]);
 	let horizontalCrease = createaCreaseLine([width/2, 0], [-width/2, 0]);
 	paper.add(verticalCrease);
-	paper.add(horizontalCrease);
+	// paper.add(horizontalCrease);
+}
+
+function showVerteces(paper) {
+	const geom = paper.userData.geom;
+	const positions = geom.attributes.position;
+
+	const pointsGeom = new THREE.BufferGeometry();
+	pointsGeom.setAttribute('position', positions.clone()); 
+
+	const pointsMat = new THREE.PointsMaterial({
+		color: 0xffffff,
+		size: 0.8,
+		sizeAttenuation: true
+	});
+
+	const vertexPoints = new THREE.Points(pointsGeom, pointsMat);
+	paper.add(vertexPoints);
+}
+
+function applyFold(paper, angleDeg) {
+  const geom = paper.userData.geom;
+  const positions = geom.attributes.position.array;
+  const rad = THREE.MathUtils.degToRad(angleDeg);
+
+  const axis = new THREE.Vector3(0, 1, 0);
+  const origin = new THREE.Vector3(0, 0, 0);
+  const rot = new THREE.Matrix4().makeRotationAxis(axis, rad);
+
+  for (let i = 0; i < positions.length; i += 3) {
+    const x = positions[i];
+    if (x > 0) {
+      const v = new THREE.Vector3(x, positions[i+1], positions[i+2]);
+      v.sub(origin).applyMatrix4(rot).add(origin);
+      positions[i] = v.x;
+      positions[i+1] = v.y;
+      positions[i+2] = v.z;
+    }
+  }
+  geom.attributes.position.needsUpdate = true;
+  geom.computeVertexNormals();
+}
+
+function foldPaper(paper, targetAngle, duration = 10.0, ease = "power2.inOut") {
+  gsap.to({ angle: 0 }, {
+    angle: targetAngle,
+    duration: duration,
+    ease: ease,
+    onUpdate: function () {
+      applyFold(paper, this.targets()[0].angle);
+    }
+  });
 }
 
 export function buildScene(scene, camera) {
-	let paper = createPaper(30);
+	let paper = createPaper(30,{showGrid:false});
+	// showVerteces(paper);
 	scene.add(paper);
 	addPlaneCreaseLines(paper);
+	// applyFold(paper, 90);
+	foldPaper(paper, 90);
 }
 
 export function update(camera) { 
