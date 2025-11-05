05839eaacacb37051e8ccfd701287fa880129d2f
Toka1000101 <tmaridasvili@protonmail.com>
2025-11-04 23:33:58 -0500

    add mesh, install gsap

--- src/main.js
+++ src/main.js
@@ -1,17 +1,39 @@
 import * as THREE from 'three';
+import gsap from 'gsap';
 
 // aspect ration of A4 paper is sqrRoot(2) : 1
 // which is approximatelly 1.414:1
 const A4size = 1.414;
-function createPaper(size) {
-	const geom = new THREE.PlaneGeometry(size, size * A4size);
+function createPaper(size, options = {}) {
+	const {
+		segments = 10,
+		showGrid = true,
+		gridColor = 0x000000,
+		paperColor = 0xFFFFFF,
+		A4size = 1.414 // √2 ≈ A4 ratio
+	} = options;
+
+	const geom = new THREE.PlaneGeometry(size, size * A4size,10,10);
 	const mat = new THREE.MeshBasicMaterial({
 		color: 0xFFFFFF,
 		side: THREE.DoubleSide,
 	});
+	
 	const paper = new THREE.Mesh(geom, mat);
-	paper.rotation.x = -Math.PI / 2;
-	return paper;
+	const paperGroup = new THREE.Group();
+	paperGroup.add(paper);
+
+	if(showGrid) {
+		const wireframe = new THREE.WireframeGeometry(geom);
+		const lineMaterial = new THREE.LineBasicMaterial({ color: gridColor });
+		const grid = new THREE.LineSegments(wireframe, lineMaterial);
+		grid.position.z = 0.01;
+		paperGroup.add(grid);
+	}	
+	paperGroup.rotation.x = -Math.PI / 2;
+
+	paperGroup.userData.geom = geom;
+	return paperGroup;
 }
 function createaCreaseLine(p1, p2) {
 	let points = [
@@ -19,20 +41,19 @@ function createaCreaseLine(p1, p2) {
 		new THREE.Vector3(p2[0], 0, p2[1]),
 	];
 	let geom = new THREE.BufferGeometry().setFromPoints(points);
-	let mat = new THREE.LineBasicMaterial({ color: 0x000000, linewidth: 1 });
+	let mat = new THREE.LineBasicMaterial({ color: 0x4527A0, linewidth: 5 });
 	let line = new THREE.Line(geom, mat);
 	line.rotation.x = Math.PI/2;
 	return line;
 }	
 
 function addPlaneCreaseLines(paper) {
-	let height = paper.geometry.parameters.height;
-	let width = paper.geometry.parameters.width;
+	let height = paper.children[0].geometry.parameters.height;
+	let width = paper.children[0].geometry.parameters.width;
 	let verticalCrease = createaCreaseLine([0, height/2],[0, -height/2]);
 	let horizontalCrease = createaCreaseLine([width/2, 0], [-width/2, 0]);
 	paper.add(verticalCrease);
 	paper.add(horizontalCrease);
-
 }
 
 export function buildScene(scene, camera) {
@@ -42,7 +63,5 @@ export function buildScene(scene, camera) {
 }
 
 export function update(camera) { 
-
 }
-
 
