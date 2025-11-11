233c0279f509e966b3d6777941daaa17985eb018
Toka1000101 <tmaridasvili@protonmail.com>
2025-11-10 19:38:12 -0500

    make animation stop at 50%

--- src/main.js
+++ src/main.js
@@ -4,26 +4,32 @@ import { lightScene } from './light';
 let boat;
 let mixer;
 const clock = new THREE.Clock();
+function playAnimationPercent(gltf, scene, percent = 0.5) {
+  const model = gltf.scene;
+  scene.add(model);
+  model.scale.set(10, 10, 10);
 
+  const mixer = new THREE.AnimationMixer(model);
+  const clip = gltf.animations[0];
+
+  const action = mixer.clipAction(clip);
+  action.setLoop(THREE.LoopOnce);
+  action.clampWhenFinished = true;
+  action.play();
+
+  const targetDuration = clip.duration * percent;
+  action.getClip().duration = targetDuration;
+
+  return mixer;
+}
 export function buildScene(scene, camera) {
-
 	lightScene(scene);
-
   const loader = new GLTFLoader();
   const url = 'assets/cartoon_paper_boat.glb';
-
   function onLoad(gltf) {
-    console.log(gltf.scene);
-    boat = gltf.scene;
-    boat.scale.set(20, 20, 20);
-    scene.add(boat);
-		console.log(gltf.animations);
-		let model = gltf.scene;
-		// model.scale.set(0.5,0.5,0.5);
-		mixer = new THREE.AnimationMixer(model);
-		const action = mixer.clipAction(gltf.animations[0]);
-    action.play();
+		mixer = playAnimationPercent(gltf, scene);
   }
+
   loader.load(
 		url, 
 		onLoad,
